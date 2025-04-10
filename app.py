44276a25247e6710083884051fa4f1a4771bc273
RafaellFlorindo <rafaelflorindodev@gmail.com>
2025-04-10 18:13:23 -0300

    Estilos CSS ajustados e projeto estruturado

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
-app = Flask(__name__)  # Cria a aplicação Flask
+app = Flask(__name__)
 
-# Configuração do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///empreguei.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evita warnings
-
-# Inicializa o banco de dados com a aplicação
-db = SQLAlchemy(app)
-
-# Rota da página inicial
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# Executa a aplicação
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/servicos')
+def servicos():
+    return render_template('servicos.html')
+
+@app.route('/cadastrar')
+def cadastrar_servico():
+    return render_template('cadastrar.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
