0eef0f16613d965817e9dda3c5d96ab9b92ad8cf
RafaellFlorindo <rafaelflorindo@gmail.com>
2025-04-15 18:39:05 -0300

    Mudando o banco de dados para PostGres

--- models/user.py
+++ models/user.py
@@ -1,9 +1,16 @@
 from flask_login import UserMixin
 from models import db
+from datetime import datetime
 
-class User(UserMixin, db.Model):
-    __tablename__ = 'usuario'
+class User(db.Model, UserMixin):
+    __tablename__ = 'usuarios'
+
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
+    tipo_usuario = db.Column(db.String(20))
+    data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.nome}>'
