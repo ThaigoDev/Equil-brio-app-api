c11756663d452f46c4f4628760e00a6cd26629eb
ThaigoDev <thaigodeveloper@gmail.com>
2025-04-17 14:37:47 -0300

    att

--- routes/api_routes.py
+++ routes/api_routes.py
@@ -12,21 +12,18 @@ def index():
 @app.route('/signup',methods = ['POST'])  
 
 def signUp ():  
- 
-
-
  try : 
-   data = request.get_json()
-   name = request.get('name')
-   email = request.get('email')
-   password = request.get('password')  
+   name = request.form.get('name')
+   email = request.form.get('email')
+   password = request.form.get('password')  
    newUser = User(name = name, email = email, password =password) 
    db.session.add(newUser) 
    db.session.commit()  
    return jsonify(
      {
        "status":200, 
-       "msg" : "user criado com sucesso", 
+       "msg" : "User criado com sucesso!",  
+       "autorized" : True 
         }
      ) 
  except Exception as e :  
@@ -34,7 +31,23 @@ def signUp ():
    return jsonify(
       {
          "status":400, 
-         "msg" : e, 
+         "msg" : str(e), 
          }
       ) 
 
+@app.route("/users/all",methods = ['GET']) 
+def getAllUsers() : 
+ allUsers =  User.query.all()   
+ result = []
+ for user in allUsers : 
+  result.append({
+   "id": user.id,
+   "name": user.name,
+   "email": user.email,
+   "password": user.password,
+  })
+ return jsonify(
+  {
+    "users" :  result
+  }
+  )
