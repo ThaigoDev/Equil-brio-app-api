e0f0a7b085815c13b91a05e7469df3de4cef5ff6
JuniorrBraga <bragajuniordev@gmail.com>
2025-05-08 17:49:07 -0300

    database no onrender adicionado

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from flask import Flask, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from database.db_setup import db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Inicialização do Flask
 app = Flask(__name__)
 
-# Configuração para PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/equilibrio'
+# Configuração para PostgreSQL (Render)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicialização do banco de dados
 db.init_app(app)
 
-# Importa os modelos após a configuração do banco de dados
+# Importa os modelos e rotas
 from models.user import User
-
-# Importa as rotas após a configuração do banco de dados
-from routes.api_routes import  *
-
-# Registrando o Blueprint
-
+from routes.api_routes import *
 
 # Criação das tabelas no banco de dados
 with app.app_context():
